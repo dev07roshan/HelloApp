962c2b538469932b90c659739fb536e7a8018413
dev07roshan <dev07roshan@gmail.com>
2026-03-25 09:14:20 +0530

    Anything

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,27 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
         
-        if (args.length > 0) {
-           
-            name = args[0];
-            
-           
-        } 
+        String finalName;
 
-         System.out.println("Hello, " + name + "!");
+        if (args.length > 0) {
+            
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            finalName = nameBuilder.toString();
+            
+        } else {
+
+            finalName = "World"; 
+        }
+
+        System.out.println("Hello, " + finalName + "!");
     }
 }
